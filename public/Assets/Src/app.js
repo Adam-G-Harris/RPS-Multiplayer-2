d728c9adfea3e5e8980a24158717081de3a0cc5a
Adam Harris <adamgrantharris14@gmail.com>
2018-03-02 19:16:55 -0600

    Starter files

---
+++ public/Assets/Src/app.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDSyZ3k33GZJKTOYipq4aUTftGGkpZCNjk",
+    authDomain: "rps-multiplayer-2.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-2.firebaseio.com",
+    projectId: "rps-multiplayer-2",
+    messagingSenderId: "299324883614"
+};
+firebase.initializeApp(config);
